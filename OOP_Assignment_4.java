62d0466426e621a04fa8155b9a86538ea5cf5dc9
TJ Yumul <132363388+TJYumul@users.noreply.github.com>
2024-03-29 18:01:22 +0800

    Add files via upload

---
+++ OOP_Assignment_4.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class OOP_Assignment_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int max_Attempts = 3;
+        int attempts = 0;
+
+        try {
+            while (attempts < max_Attempts) {
+                System.out.print("Enter password: ");
+                String password = sc.nextLine();
+                try {
+                    validation(password);
+                    System.out.println("Login successful!");
+                    break;
+                } catch (InvalidPasswordException e) {
+                    System.out.println(e.getMessage());
+                    attempts++;
+                }
+            }
+            if (attempts == max_Attempts) {
+                throw new MaxLoginTriesException("Maximum login attempts reached.");
+            }
+        } catch (MaxLoginTriesException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void validation(String pass) throws MaxLoginTriesException, InvalidPasswordException {
+        final String valid = "password";
+
+        if (!pass.equals(valid)) {
+            throw new InvalidPasswordException("Invalid password.");
+        }
+    }
+
+    static class MaxLoginTriesException extends Exception {
+        public MaxLoginTriesException(String message) {
+            super(message);
+        }
+    }
+
+    static class InvalidPasswordException extends Exception {
+        public InvalidPasswordException(String message) {
+            super(message);
+        }
+    }
+}
